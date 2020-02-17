dc9cc562c64c13cdd3c509f2bcc43ce893d2d060
NicolasDutour <dutourn99@gmail.com>
2020-02-17 15:45:24 +0100

    add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
